09ea7fe9f1360b28f7609ff57b78c9e7fc242893
lkorzechowski <lkorzechowski@gmail.com>
2021-09-03 09:43:12 +0200

    Initial commit

---
+++ src/main/java/com/orzechowski/aidme/AidMeApplication.java
@@ -0,0 +1,13 @@
+package com.orzechowski.aidme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidMeApplication.class, args);
+	}
+
+}
