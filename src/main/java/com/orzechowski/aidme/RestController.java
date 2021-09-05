659c7c5eef535217c9c20b7f2a1d45e69556de02
lkorzechowski <lkorzechowski@gmail.com>
2021-09-05 22:02:46 +0200

    0.02 some basic mappings

--- src/main/java/com/orzechowski/aidme/RestController.java
+++ src/main/java/com/orzechowski/aidme/RestController.java
@@ -1,6 +1,10 @@
 package com.orzechowski.aidme;
 
+import com.orzechowski.aidme.entities.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController
@@ -9,5 +13,40 @@ public class RestController
     public String home()
     {
         return "";
+    }
+
+    @PostMapping("/helpers")
+    public List<Helper> helpers()
+    {
+        List<Helper> helpers = null;
+        return helpers;
+    }
+
+    @PostMapping("/tutorials")
+    public List<Tutorial> tutorials()
+    {
+        List<Tutorial> tutorials = null;
+        return tutorials;
+    }
+
+    @PostMapping("/categories")
+    public List<Category> categories()
+    {
+        List<Category> categories = null;
+        return categories;
+    }
+
+    @PostMapping("/tags")
+    public List<Tag> tags()
+    {
+        List<Tag> tags = null;
+        return tags;
+    }
+
+    @PostMapping("/keywords")
+    public List<Keyword> keywords()
+    {
+        List<Keyword> keywords = null;
+        return keywords;
     }
 }
