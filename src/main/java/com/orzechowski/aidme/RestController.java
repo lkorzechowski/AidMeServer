79268816a235ff14ed5e8dfbb1dfc9d0b736f212
lkorzechowski <lkorzechowski@gmail.com>
2021-09-09 20:54:06 +0200

    0.03 jdbc connecting to my postgresql db

--- src/main/java/com/orzechowski/aidme/RestController.java
+++ src/main/java/com/orzechowski/aidme/RestController.java
@@ -1,6 +1,9 @@
 package com.orzechowski.aidme;
 
 import com.orzechowski.aidme.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,6 +12,9 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 public class RestController
 {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String home()
     {
@@ -18,35 +24,30 @@ public class RestController
     @PostMapping("/helpers")
     public List<Helper> helpers()
     {
-        List<Helper> helpers = null;
-        return helpers;
+        return jdbcTemplate.query("SELECT * FROM helpers", BeanPropertyRowMapper.newInstance(Helper.class));
     }
 
     @PostMapping("/tutorials")
     public List<Tutorial> tutorials()
     {
-        List<Tutorial> tutorials = null;
-        return tutorials;
+        return jdbcTemplate.query("SELECT * FROM tutorials", BeanPropertyRowMapper.newInstance(Tutorial.class));
     }
 
     @PostMapping("/categories")
     public List<Category> categories()
     {
-        List<Category> categories = null;
-        return categories;
+        return jdbcTemplate.query("SELECT * FROM categories", BeanPropertyRowMapper.newInstance(Category.class));
     }
 
     @PostMapping("/tags")
     public List<Tag> tags()
     {
-        List<Tag> tags = null;
-        return tags;
+        return jdbcTemplate.query("SELECT * FROM tags", BeanPropertyRowMapper.newInstance(Tag.class));
     }
 
     @PostMapping("/keywords")
     public List<Keyword> keywords()
     {
-        List<Keyword> keywords = null;
-        return keywords;
+        return jdbcTemplate.query("SELECT * FROM keywords", BeanPropertyRowMapper.newInstance(Keyword.class));
     }
 }
